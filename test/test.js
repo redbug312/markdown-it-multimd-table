dd4707e27ba45a046caa15b54c8af2afe4671af9
RedBug312 <redbug312@gmail.com>
2019-05-22 13:02:18 +0800

    Disable no-bitwise lint rule; Add lint ignore
    According to eslint [docs], no-bitwise rule is set to avoid confusion of
    bitwise operators and logical operators, and under the assumption that
    bitwise is rarely used. Considering the importance in this plugin, I
    have to disable this rule for readability, in cost of consistence with
    markdown-it lint rules.
    
    There's no intention to have linting on test/, so it's added to .eslintignore
    
    [docs]: https://eslint.org/docs/rules/no-bitwise

--- test/test.js
+++ test/test.js
@@ -4,31 +4,31 @@ var generate = require('markdown-it-testgen');
 
 /* eslint-env mocha */
 
-describe('Basic', function() {
+describe('Basic', function () {
   var md = require('markdown-it')()
               .use(require('../'));
   generate(path.join(__dirname, 'fixtures/basic.txt'), md);
 });
 
-describe('Requirements', function() {
+describe('Requirements', function () {
   var md = require('markdown-it')()
               .use(require('../'));
   generate(path.join(__dirname, 'fixtures/requirements.txt'), md);
 });
 
-describe('Other Notes', function() {
+describe('Other Notes', function () {
   var md = require('markdown-it')()
               .use(require('../'));
   generate(path.join(__dirname, 'fixtures/notes.txt'), md);
 });
 
-describe('Issues', function() {
+describe('Issues', function () {
   var md = require('markdown-it')()
               .use(require('../'));
   generate(path.join(__dirname, 'fixtures/issues.txt'), md);
 });
 
-describe('(optional) Multilines', function() {
+describe('(optional) Multilines', function () {
   var md = require('markdown-it')()
               .use(require('../'), { enableMultilineRows: true });
   generate(path.join(__dirname, 'fixtures/multilines.txt'), md);
