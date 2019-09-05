0c1fba650a84942324bfef18085913bb81ebe3bc
RedBug312 <redbug312@gmail.com>
2019-09-05 16:12:46 +0800

    Merge basic, notes, requirements test files
    Into standard file. These test cases are described by MultiMarkdown 5
    standard and can be hardly changed.

--- test/test.js
+++ test/test.js
@@ -1,41 +1,30 @@
 'use strict';
+
 var path     = require('path');
 var generate = require('markdown-it-testgen');
 
 /* eslint-env mocha */
 
-describe('Basic', function () {
+describe('markdown-it-multimd-table-standard', function () {
   var md = require('markdown-it')()
               .use(require('../'));
-  generate(path.join(__dirname, 'fixtures/basic.txt'), md);
-});
-
-describe('Requirements', function () {
-  var md = require('markdown-it')()
-              .use(require('../'));
-  generate(path.join(__dirname, 'fixtures/requirements.txt'), md);
-});
-
-describe('Other Notes', function () {
-  var md = require('markdown-it')()
-              .use(require('../'));
-  generate(path.join(__dirname, 'fixtures/notes.txt'), md);
+  generate(path.join(__dirname, 'fixtures/standard.txt'), {header: true}, md);
 });
 
 describe('Issues', function () {
   var md = require('markdown-it')()
               .use(require('../'));
-  generate(path.join(__dirname, 'fixtures/issues.txt'), md);
+  generate(path.join(__dirname, 'fixtures/issues.txt'), {header: true}, md);
 });
 
 describe('(optional) Multilines', function () {
   var md = require('markdown-it')()
               .use(require('../'), { enableMultilineRows: true });
-  generate(path.join(__dirname, 'fixtures/multilines.txt'), md);
+  generate(path.join(__dirname, 'fixtures/multilines.txt'), {header: true}, md);
 });
 
 describe('(optional) Rowspans', function () {
   var md = require('markdown-it')()
               .use(require('../'), { enableMultilineRows: true, enableRowspan: true });
-  generate(path.join(__dirname, 'fixtures/rowspan.txt'), md);
+  generate(path.join(__dirname, 'fixtures/rowspan.txt'), {header: true}, md);
 });
