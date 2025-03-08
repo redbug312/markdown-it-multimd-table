35173ae5c7891fccff5ce67a5da5f1b5fc2e6588
redbug312 <redbug312@gmail.com>
2025-03-08 23:10:10 +0800

    fixup

--- test/test.mjs
+++ test/test.mjs
@@ -5,25 +5,25 @@ import { fileURLToPath } from 'node:url'
 import multimd_table from '../index.mjs'
 
 describe('markdown-it-multimd-table-standard', function () {
-  const md = markdownit().use(multimd_table);
+  const md = markdownit().use(multimd_table)
   const url = fileURLToPath(new URL('fixtures/standard.txt', import.meta.url))
-  generate(url, { header: true }, md);
-});
+  generate(url, { header: true }, md)
+})
 
 describe('markdown-it-multimd-table-undefined', function () {
-  const md = markdownit().use(multimd_table);
+  const md = markdownit().use(multimd_table)
   const url = fileURLToPath(new URL('fixtures/unspecified.txt', import.meta.url))
-  generate(url, { header: true }, md);
-});
+  generate(url, { header: true }, md)
+})
 
 describe('markdown-it-multimd-table-optional', function () {
   const md = markdownit().use(multimd_table, {
-    multiline:  true,
-    rowspan:    true,
+    multiline: true,
+    rowspan: true,
     headerless: true,
-    multibody:  false,
-    autolabel:  false,
-  });
+    multibody: false,
+    autolabel: false
+  })
   const url = fileURLToPath(new URL('fixtures/options.txt', import.meta.url))
-  generate(url, { header: true }, md);
-});
+  generate(url, { header: true }, md)
+})
