8f87e8f335ef35198439ef96c5065d3be335a4bc
redbug312 <redbug312@gmail.com>
2025-03-08 23:48:11 +0800

    Apply latest ESLint rules
    The ESLint rules are now updated with markdown-it v14.1.0.

--- test/test.js
+++ test/test.js
@@ -1,30 +1,30 @@
-'use strict';
+'use strict'
 
-var path     = require('path');
-var generate = require('markdown-it-testgen');
+const path     = require('path')
+const generate = require('markdown-it-testgen')
 
-var markdownit    = require('markdown-it');
-var multimd_table = require('../');
+const markdownit    = require('markdown-it')
+const multimd_table = require('../')
 
 /* eslint-env mocha */
 
 describe('markdown-it-multimd-table-standard', function () {
-  var md = markdownit().use(multimd_table);
-  generate(path.join(__dirname, 'fixtures/standard.txt'), { header: true }, md);
-});
+  const md = markdownit().use(multimd_table)
+  generate(path.join(__dirname, 'fixtures/standard.txt'), { header: true }, md)
+})
 
 describe('markdown-it-multimd-table-unspecified', function () {
-  var md = markdownit().use(multimd_table);
-  generate(path.join(__dirname, 'fixtures/unspecified.txt'), { header: true }, md);
-});
+  const md = markdownit().use(multimd_table)
+  generate(path.join(__dirname, 'fixtures/unspecified.txt'), { header: true }, md)
+})
 
 describe('markdown-it-multimd-table-options', function () {
-  var md = markdownit().use(multimd_table, {
-    multiline:  true,
-    rowspan:    true,
+  const md = markdownit().use(multimd_table, {
+    multiline: true,
+    rowspan: true,
     headerless: true,
-    multibody:  false,
-    autolabel:  false,
-  });
-  generate(path.join(__dirname, 'fixtures/options.txt'), { header: true }, md);
-});
+    multibody: false,
+    autolabel: false
+  })
+  generate(path.join(__dirname, 'fixtures/options.txt'), { header: true }, md)
+})
